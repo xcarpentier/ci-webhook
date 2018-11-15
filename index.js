46b0d512808f3fe4af83c59d4cb9f3ad53e62a78
Xavier Carpentier <xcapetir@gmail.com>
2018-11-15 01:08:08 +0100

    fix tests

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const axios = require('axios')
 const micro = require('micro')
 const Octokat = require('octokat')
 
@@ -7,7 +8,10 @@ function extractIssueNumber(url) {
 }
 
 function expoUrlForCommit(commit) {
-  return `https://expo.io/@watchdog-system/watchdog-app?release-channel=${commit}`
+  return `https://expo.io/@watchdog-system/watchdog-app?release-channel=${commit.replace(
+    '/',
+    '_',
+  )}`
 }
 
 const octo = new Octokat({ token: process.env.API_TOKEN })
@@ -15,20 +19,52 @@ const octo = new Octokat({ token: process.env.API_TOKEN })
 const server = micro(async (req, res) => {
   const json = await micro.json(req)
   const commit = json.payload.branch
+  const body = json.payload.body
+  const committer_name = json.payload.committer_name
+  const committer_email = json.payload.committer_email
   const prUrl = json.payload.pull_requests[0].url
   if (json.payload.status === 'success' && prUrl) {
-    const issueId = extractIssueNumber(json.payload.pull_requests[0].url)
+    const issueId = extractIssueNumber(prUrl)
     const expoUrl = expoUrlForCommit(commit)
 
-    const qrCodeUrl = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${encodeURIComponent(
-      expoUrl,
-    )}`
+    const qrCodeUrl = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${expoUrl}`
     const body = `watchdog-system for ${commit} has been deployed.\n\n![](${qrCodeUrl})\n${expoUrl}`
 
     await octo
       .repos('xcarpentier', 'watchdog-system')
       .issues(issueId)
       .comments.create({ body })
+
+    await axios({
+      method: 'POST',
+      url:
+        'https://hooks.slack.com/services/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',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      data: `payload=${encodeURIComponent(
+        JSON.stringify({
+          text: `*Watchdog-System* APP for *${commit}* has been *deployed*! 🎉`,
+          channel: 'watchdog-ext',
+          username: 'bot',
+          icon_emoji: ':iphone:',
+          attachments: [
+            {
+              color: '#36a640',
+              title: `Link to the new ${commit} app version`,
+              title_link: expoUrl,
+              image_url: qrCodeUrl,
+              ts: new Date().getTime() / 1000,
+            },
+            {
+              color: '#FECC33',
+              title: body,
+              author_name: `${committer_name} <${committer_email}>`,
+            },
+          ],
+        }),
+      )}`,
+    })
   }
 
   return 'ok'
